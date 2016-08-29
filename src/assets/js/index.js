2dbaa0f7f026796d9800953e7e63ce0193f8a2eb
zjq <467446615@qq.com>
2016-08-29 19:26:27 +0800

    关于我们

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -11,18 +11,31 @@ $(document).ready(function() {
         './img/banner04.png',
         './img/banner05.png'
     ];
-    imgLoad(bannerSrc, '#myslider');
+    if ($('#myslider')[0]) {
+        imgLoad(bannerSrc, '#myslider');
+        var slider = new SimpleSlider(document.getElementById('myslider'), {
+            autoPlay: true,
+            transitionProperty: 'opacity',
+            transitionDuration: 2,
+            transitionDelay: 5,
+            startValue: 0,
+            visibleValue: 1,
+            endValue: 0,
+            onChange: null,
+            onChangeEnd: null
+        });
+    }
 
     function windowResize() {
-        var h = $('#myslider img').height();
-        var m = $('#myslider').css('margin-left');
-        $('#myslider').height(h);
+        var h = $('.banner img').height();
+        var m = $('.banner').css('margin-left');
+        $('.banner').height(h);
         $('.news').css('left', m);
     }
     $(window).resize(function windowResize() {
-        var h = $('#myslider img').height();
-        var m = $('#myslider').css('margin-left');
-        $('#myslider').height(h);
+        var h = $('.banner img').height();
+        var m = $('.banner').css('margin-left');
+        $('.banner').height(h);
         $('.news').css('left', m);
     });
     windowResize();
@@ -32,15 +45,4 @@ $(document).ready(function() {
     $('.nav-solution,.nav-product,.nav-contact').on('mouseleave', function() {
         $(this).children().removeClass('active');
     });
-});
-var slider = new SimpleSlider(document.getElementById('myslider'), {
-    autoPlay: true,
-    transitionProperty: 'opacity',
-    transitionDuration: 2,
-    transitionDelay: 5,
-    startValue: 0,
-    visibleValue: 1,
-    endValue: 0,
-    onChange: null,
-    onChangeEnd: null
 });
