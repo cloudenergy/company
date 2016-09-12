1126e9acd842b9b72fea29f93cccdd9be472a54d
zjq <467446615@qq.com>
2016-09-12 16:47:51 +0800

    冗余部分删除

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,38 +1,15 @@
-$(document).ready(function() {
-    function imgLoad() {
-        var img = $('img[origin-src]');
-        for (var i = 0; i < img.length; i++) {
-            img[i].src = img[i].attributes['origin-src'].value;
-        }
+function imgLoad() {
+    var img = $('img[origin-src]');
+    for (var i = 0; i < img.length; i++) {
+        img[i].src = img[i].attributes['origin-src'].value;
     }
-    if ($('#myslider')[0]) {
-        imgLoad();
-        var slider = new SimpleSlider(document.getElementById('myslider'), {
-            autoPlay: true,
-            transitionProperty: 'opacity',
-            transitionDuration: 2,
-            transitionDelay: 5,
-            startValue: 0,
-            visibleValue: 1,
-            endValue: 0,
-            onChange: null,
-            onChangeEnd: null
-        });
-    }
-
-    function windowResize() {
-        var h = $('.inner-height').width();
-        $('.inner-height').height(h * 1000 / 1920);
-    }
-    $(window).resize(function() {
-        var h = $('.inner-height').width();
-        $('.inner-height').height(h * 1000 / 1920);
-    });
-    windowResize();
+}
+window.onload = function() {
+    imgLoad();
     $('.nav-solution,.nav-product,.nav-contact').on('mouseenter', function() {
         $(this).children().addClass('active');
     });
     $('.nav-solution,.nav-product,.nav-contact').on('mouseleave', function() {
         $(this).children().removeClass('active');
     });
-});
+}
