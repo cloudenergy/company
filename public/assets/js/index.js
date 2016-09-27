611956c81adc29bdd6f33fe0f1ea16649d591deb
zjq <467446615@qq.com>
2016-09-27 11:00:46 +0800

    修复banner下边空白

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,12 +4,22 @@ function imgLoad() {
         img[i].src = img[i].attributes['origin-src'].value;
     }
 }
-window.onload = function() {
+window.onload = function () {
     imgLoad();
-    $('.nav-solution,.nav-product,.nav-contact').on('mouseenter', function() {
+    $('.nav-solution,.nav-product,.nav-contact').on('mouseenter', function () {
         $(this).children().addClass('active');
     });
-    $('.nav-solution,.nav-product,.nav-contact').on('mouseleave', function() {
+    $('.nav-solution,.nav-product,.nav-contact').on('mouseleave', function () {
         $(this).children().removeClass('active');
     });
+};
+
+function windowResize() {
+    var h = $('.inner-height').width();
+    $('.inner-height').height(h * 1000 / 1920);
 }
+$(window).resize(function () {
+    var h = $('.inner-height').width();
+    $('.inner-height').height(h * 1000 / 1920);
+});
+windowResize();
